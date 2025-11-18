751bda5dfffeaf02c047da9034ea4c3649207a4e
Zlarma <danbrakerptbr@gmail.com>
2025-11-18 08:33:40 -0300

    Teste 2

--- back/controller/usuario.controller.js
+++ back/controller/usuario.controller.js
@@ -1,19 +1,32 @@
-const Usuario = require('../models/Usuario')
+const Usuario = require("../models/Usuario");
+const { hashSenha } = require("../service/bcrypt.service");
 
-const cadastrar = async (req,res)=>{
-    const valores = req.body
-    
-    if( !valores.nome || !valores.senha){
-        return res.status(400).json({message: 'Todos os campos são obrigatórios!'})
+const cadastrar = async (req, res) => {
+  const valores = req.body;
+
+  if (
+    !valores.nome ||
+    !valores.email ||
+    !valores.senha ||
+    !valores.telefone ||
+    !valores.cpf ||
+    !valores.tipo_usuario
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Todos os campos são obrigatórios!" });
+  }
+
+  try {
+    if (valores.senha) {
+      valores.senha = await hashSenha(valores.senha);
     }
+    const dados = await Usuario.create(valores);
+    res.status(201).json(dados);
+  } catch (err) {
+    console.error("Erro ao cadastrar os dados", err);
+    res.status(500).json({ message: "Erro ao cadastrar os dados!" });
+  }
+};
 
-    try{
-        const dados = await Usuario.create(valores)
-        res.status(201).json(dados)
-    }catch(err){
-        console.error('Erro ao cadastrar os dados',err)
-        res.status(500).json({message: "Erro ao cadastrar os dados!"})
-    }
-}
-
-module.exports = { cadastrar }
+module.exports = { cadastrar };
