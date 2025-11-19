d3508785a547afd6132440ccf34201a7c20c48c7
DANIEL DOMINGOS PEREIRA <daniel_d_pereira@estudante.sesisenai.org.br>
2025-11-19 12:25:53 -0300

    noalt

--- back/db/conn.js
+++ back/db/conn.js
@@ -1,23 +1,24 @@
-const { Sequelize } = require('sequelize')
-require('dotenv').config()  // carregar as variáveis de ambiente
+const { Sequelize } = require("sequelize");
+require("dotenv").config(); // carregar as variáveis de ambiente
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql'
-    }
-)
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
 
-sequelize.authenticate()
-.then(() => {
-    console.log('Conexão realizada com sucesso!')
-})
-.catch((error) => {
-    console.error('Erro ao conectar com banco de dados!', error)
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexão realizada com sucesso!");
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar com banco de dados!", error);
+  });
 
-module.exports = sequelize
+module.exports = sequelize;
